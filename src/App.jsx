b1f4c18176180b347862825043b2877ae9408260
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-03-04 22:49:48 +0100

    Add header & .scores-container layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <>
-      <h1>Memory Cards</h1>
+        <header>
+            <h1>Memory Cards</h1>
+            <div className="scores-container">
+                <p>Score: 0</p>
+                <p>Best: 0</p>
+            </div>
+        </header>
 
       <div className="cards-container"></div>
 
@@ -9,6 +15,7 @@ function App() {
         href="https://github.com/bartek8b"
         target="_blank"
         rel="noopener noreferrer"
+        className="signature"
       >
         Created by Bartlomiej Balcerzak
       </a>
