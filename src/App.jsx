56b181f9249265dd84a649c91b144f089b45b8c1
bartek8b <163062773+bartek8b@users.noreply.github.com>
2026-03-04 22:20:35 +0100

    Set basic layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-
-import './App.css'
-
 function App() {
+  return (
+    <>
+      <h1>Memory Cards</h1>
 
+      <div className="cards-container"></div>
+
+      <a
+        href="https://github.com/bartek8b"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Created by Bartlomiej Balcerzak
+      </a>
+    </>
+  );
 }
 
-export default App
+export default App;
